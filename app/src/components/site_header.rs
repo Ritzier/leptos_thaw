7c056caf138d1ee4e37dfbf01b7e183c43571f34
ritzier <ritzierwheat@gmail.com>
2024-12-13 10:55:21 +0800

    feat(app): modify `app/src/components/site_header.rs`

--- app/src/components/site_header.rs
+++ app/src/components/site_header.rs
@@ -1,37 +1,20 @@
 use leptos::prelude::*;
+use leptos_router::hooks::use_navigate;
 use thaw::*;
 
+// Initalize, HomeButton
 #[component]
-pub fn SwitchVersion() -> impl IntoView {
-    let options = vec![("main", "https://thawui.vercel.app")];
-
-    let version = RwSignal::new(None::<String>);
-    let label = RwSignal::new(String::new());
-    #[cfg(any(feature = "hydrate"))]
-    {
-        let location = window().location();
-        let origin = location.origin().ok();
-        if let Some(origin) = origin.as_ref() {
-            if let Some(item) = options.iter().find(|item| item.1 == origin) {
-                label.set(item.0.to_string());
-            }
-        }
-        version.set(origin);
-        let _ = version.watch(move |origin| {
-            if let Some(origin) = origin {
-                let pathname = location.pathname().unwrap_or_default();
-                let href = format!("{}{}", origin, pathname);
-                let _ = location.set_href(&href);
-            }
-        });
-    }
+pub fn SiteHeader() -> impl IntoView {
+    let navigate = use_navigate();
 
     view! {
-        <Combobox value=label selected_options=version placeholder="Switch version">
-            {options
-                .into_iter()
-                .map(|option| view! { <ComboboxOption value=option.1 text=option.0 /> })
-                .collect_view()}
-        </Combobox>
+        <LayoutHeader class="demo-header">
+            <Space on:click=move |_| {
+                navigate("/", Default::default());
+            }>
+                <img src="/logo.svg" style="width: 36px" />
+                <div class="demo-name">"Thaw UI"</div>
+            </Space>
+        </LayoutHeader>
     }
 }
