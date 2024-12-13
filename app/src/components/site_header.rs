f91858a9ed2169f0704655eb5477454a8d7813cb
ritzier <ritzierwheat@gmail.com>
2024-12-13 11:56:16 +0800

    feat(app): add `<Style` to `app/src/components/site_header.rs`

--- app/src/components/site_header.rs
+++ app/src/components/site_header.rs
@@ -8,6 +8,51 @@ pub fn SiteHeader() -> impl IntoView {
     let navigate = use_navigate();
 
     view! {
+        <Style id="demo-header">
+            "
+            .demo-header {
+                height: 64px;
+                display: flex;
+                align-items: center;
+                justify-content: space-between;
+                padding: 0 20px;
+                z-index: 1000;
+                position: relative;
+                border-bottom: 1px solid var(--colorNeutralStroke2);
+            }
+            .demo-name {
+                cursor: pointer;
+                display: flex;
+                align-items: center;
+                height: 100%;
+                font-weight: 600;
+                font-size: 20px;
+            }
+            .demo-header__menu-mobile {
+                display: none !important;
+            }
+            .demo-header__right-btn .thaw-select {
+                width: 60px;
+            }
+            @media screen and (max-width: 600px) {
+                .demo-header {
+                    padding: 0 8px;
+                }
+                .demo-name {
+                    display: none;
+                }
+            }
+            @media screen and (max-width: 1200px) {
+                .demo-header__right-btn {
+                    display: none !important;
+                }
+                .demo-header__menu-mobile {
+                    display: inline-block !important;
+                }
+            }
+            "
+        </Style>
+
         <LayoutHeader class="demo-header">
             <Space on:click=move |_| {
                 navigate("/", Default::default());
