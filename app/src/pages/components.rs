bb43aad2b9ae03a3ec10b738ca912eb25be6d8d8
ritzier <ritzierwheat@gmail.com>
2024-12-13 11:06:06 +0800

    feat(app): modify `app/src/pages/components.rs`

--- app/src/pages/components.rs
+++ app/src/pages/components.rs
@@ -1,1 +1,13 @@
-use std::vec;
+use leptos::prelude::*;
+use thaw::*;
+
+use crate::components::SiteHeader;
+
+#[component]
+pub fn ComponentsPage() -> impl IntoView {
+    view! {
+        <Layout position=LayoutPosition::Absolute>
+            <SiteHeader />
+        </Layout>
+    }
+}
